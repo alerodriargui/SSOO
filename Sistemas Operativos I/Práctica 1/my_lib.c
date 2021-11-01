9df3caec6180f9cfe3bd154fc3412756323b91a5
nicost28 <76444007+nicost28@users.noreply.github.com>
2021-11-01 20:19:25 +0100

    Add files via upload

--- Sistemas Operativos I/Práctica 1/my_lib.c
+++ Sistemas Operativos I/Práctica 1/my_lib.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+struct my_stack_node {      // nodo de la pila (elemento)
+  void *data;
+  struct my_stack_node *next;
+};
+ 
+struct my_stack {   // pila
+  int size;       // tamaño de data, nos lo pasarán por parámetro
+  struct my_stack_node *top;  // apunta al nodo de la parte superior
+};
+
 size_t my_strlen(const char *str)
 {
     size_t len = 0;
@@ -85,4 +95,95 @@ char *my_strchr(const char *str, int c)
     }
     //Si no hay ninguna coincidencia se de NULL
     return NULL;
+}
+
+//Método que reserva espacio para variable struct my_stack
+struct my_stack*my_stack_init(int size)
+{
+    //Creación de variable y reserva de espacio
+    struct my_stack* Pila = malloc(sizeof(struct my_stack));
+    //Campo size = datos pasados por parámetro
+    Pila->size=size;
+    //Puntero top apuntando a NULL
+    Pila->top=NULL;   
+
+    //Devolver Pila
+    return Pila;
+}
+
+//Método que inserta nodo en la pila
+int my_stack_push (struct my_stack *stack, void *data)
+{
+    //Llamar método my_stack_init a stack para reservar espacio
+    stack = my_stack_init(sizeof(struct my_stack_node));
+
+    //Si stack == NULL...
+    if(stack==NULL){
+        //Se devuelve -1 (EXIT_FAILURE)
+        return -1;
+    }
+
+    //Si campo size del stack es igual o menor que 0...
+    else if ((stack->size=0) || (stack->size<0))
+    {
+        //Se devuelve -1 (EXIT_FAILURE)
+        return -1;
+    }
+
+    //Y si stack != NULL y campo size mayor que 0...
+    else if ((stack!=NULL) && (stack->size>0))
+    {
+        //Se podrá insertar el nodo
+       //Creación Nodo
+        struct my_stack_node* Nodo = malloc(sizeof(struct my_stack_node));
+        //Campo data del nodo igual al dado por parámetro
+        Nodo->data=data;
+        //Y el nodo apunta adonde apunta top
+        Nodo->next=stack->top;
+        //Y el top de la pila ahora va a apuntar al nodo insertado
+        stack->top=Nodo;
+
+        //Se devuelve 0 (EXIT_SUCCESS)
+        return 0;
+    }
+}
+
+//Método que elimina nodo superior pila
+void *my_stack_pop (struct my_stack *stack)
+{
+    //Si pila vacía...
+    if(stack==NULL){
+        //Devuelve NULL
+        return NULL;
+    }
+    //Sino
+    else{
+        //Puntero pila apunta donde apunta nodo superior
+        stack->top=stack->top->next;
+        //Y nodo superior apunta ahora a NULL para desaparecer de la pila
+        stack->top->next=NULL;
+    }
+    
+}
+
+//Método que cuenta cuantos nodos hay
+int my_stack_len (struct my_stack *stack)
+{
+    //Si pila vacía...
+    if(stack==NULL){
+        //Se devuelve 0
+        return 0;
+    }
+    //Sino
+    else{
+        int i;
+        //Se recorre la pila hasta que el puntero de la misma sea igual a NULL
+        for(int i = 1; stack->top!=NULL;i++){
+            //Pila apunta al siguiente elemento
+            stack->top=stack->top->next;
+         }
+         //Se devuelve num. de nodos
+         return i;
+    }
+    
 }
